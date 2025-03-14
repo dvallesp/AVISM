baa30e4976f00d010906c403f3e4e5bed84af1ed
Óscar Monllor Berbegal <78754527+oscarmonllor99@users.noreply.github.com>
2025-03-14 11:44:14 +0100

    Added read region possibility in read_voids.py

--- tools/read_voids.py
+++ tools/read_voids.py
@@ -1,7 +1,6 @@
 """
 read_voids module
-Provides a function to read the void catologue and a function to read
-the 3D void structure outputs
+Provides functions to read the void catologue and 3D void shapes from AVISM
 
 Created by Óscar Monllor and Mónica Hernández
 """
@@ -13,14 +12,20 @@ from scipy.io import FortranFile as FF
 
 # FUNCTIONS DEFINED IN THIS MODULE:
 
-def read_void_catalogue(it, path='', output_format = 'dictionaries', ret_details=True, ret_catalogue=True):
+def read_void_catalogue(it, path='', output_format = 'dictionaries', read_region=None,
+                        ret_details=True, ret_catalogue=True):
     """
-    Reads the voidXXXXX files, containing the void catalogue of the MASCLET void finder.
+    Reads the voidXXXXX files, containing the void catalogue of AVISM.
     
     Args:
         - it: iteration number (int)
         - path: path of the stellar_haloes (str)
         - output_format: 'dictionaries' or 'arrays'
+        - read_region: whether to select a subregion (see region specification below), or keep all data (None)
+             If a region wants to be selected, choose one of the following possibilities:
+            - ("sphere", cx, cy, cz, R) for a sphere of radius R centered in (cx, cy, cz)
+            - ("box", x1, x2, y1, y2, z1, z2) for a box with corners (x1, y1, z1) and (x2, y2, z2)
+            - ("box_cw", xc, yc, zc, width) for a box centered in (xc, yc, zc) with width "width"
 
     Returns:
         - if ret_details=True: dictionary with voidfinder details
@@ -28,8 +33,9 @@ def read_void_catalogue(it, path='', output_format = 'dictionaries', ret_details
             - if output_format='dictionaries': list of dictionaries, one per void
             - if output_format='arrays': dictionary of arrays, one array per void property
         - if both are true: dictionary with voidfinder details and list of dictionaries, one per void level
+        - if read_region is not None: list of voids intersecting the region (nvoids not updated)
     """
-    
+
     #Check output format
     if output_format not in ['dictionaries', 'arrays']:
         raise ValueError('output_format must be "dictionaries" or "arrays"')
@@ -69,12 +75,52 @@ def read_void_catalogue(it, path='', output_format = 'dictionaries', ret_details
                 this_level['nparents'] = nparents
                 this_level['FF'] = FF
                 
+                # read region
+                if read_region is not None:
+                    # array marking cells in voids
+                    mapOutput = read_void_map(it, path, output_marca=True, output_deltaTot=False, output_div=False)
+                    marcaLev = mapOutput[0][ilev]
+                    # mask of region
+                    mask = np.zeros_like(marcaLev, dtype=int)
+                    nx_lev = nmax * 2**(lev-levmin)
+                    ny_lev = nmay * 2**(lev-levmin)
+                    nz_lev = nmaz * 2**(lev-levmin)
+                    dx_lev = L0 / nx_lev
+                    dy_lev = L0 / ny_lev
+                    dz_lev = L0 / nz_lev
+                    x = np.arange(-L0/2+dx_lev/2, L0/2, dx_lev)
+                    y = np.arange(-L0/2+dy_lev/2, L0/2, dy_lev)
+                    z = np.arange(-L0/2+dz_lev/2, L0/2, dz_lev)
+                    X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+
+                    region_type = read_region[0]
+                    if region_type == 'sphere':
+                        cx, cy, cz, R = read_region[1:]
+                        mask = (X-cx)**2 + (Y-cy)**2 + (Z-cz)**2 < R**2
+
+                    elif region_type == 'box':
+                        x1, x2, y1, y2, z1, z2 = read_region[1:]
+                        mask = (X >= x1) & (X <= x2) & (Y >= y1) & (Y <= y2) & (Z >= z1) & (Z <= z2)
+
+                    elif region_type == 'box_cw':
+                        xc, yc, zc, width = read_region[1:]
+                        mask = (np.abs(X-xc) < width) & (np.abs(Y-yc) < width) & (np.abs(Z-zc) < width)
+
+                    else:
+                        raise ValueError('Error! region_type must be either "sphere", "box" or "box_cw"')
+
+                    # mask voids inside region
+                    voids_inside = np.unique(marcaLev[mask])    
+
                 # Void data
                 voids=[]
                 for iv in range(nvoids):
                     void = {}
                     data_line = np.array(void_catalogue.readline().split()).astype('f4')
                     void['id'] = int(data_line[0])
+                    if read_region is not None:
+                        if void['id'] not in voids_inside:
+                            continue
                     void['xc'] = data_line[1]
                     void['yc'] = data_line[2]
                     void['zc'] = data_line[3]
@@ -95,6 +141,8 @@ def read_void_catalogue(it, path='', output_format = 'dictionaries', ret_details
                     this_level['voids'] = voids
                 elif output_format=='arrays':
                     this_level['voids'] = {k: np.array([v[k] for v in voids]) for k in voids[0].keys()}
+                else:
+                    raise ValueError('Error! output_format argument should be either dictionaries or arrays')
 
                 levels.append(this_level)
 
@@ -107,15 +155,19 @@ def read_void_catalogue(it, path='', output_format = 'dictionaries', ret_details
         return void_details
 
 
-def read_cubes(it, path='', output_format = 'dictionaries'):
+def read_cubes(it, path='', output_format = 'dictionaries', read_region=None):
     """
-    Reads the cubesXXXXX files, containing all cubes making up the final void catalogue
+    Reads the cubesXXXXX files, containing all cubes making up the final AVISM (main) voids
 
     Args:
         it: iteration number (int)
         path: path of the grids file in the system (str)
         output_format: 'dictionaries' or 'arrays'   
-
+        read_region: whether to select a subregion (see region specification below), or keep all data (None)
+             If a region wants to be selected, choose one of the following possibilities:
+            - ("sphere", cx, cy, cz, R) for a sphere of radius R centered in (cx, cy, cz)
+            - ("box", x1, x2, y1, y2, z1, z2) for a box with corners (x1, y1, z1) and (x2, y2, z2)
+            - ("box_cw", xc, yc, zc, width) for a box centered in (xc, yc, zc) with width "width"
     Returns:
         List of dictionaries, one per level, containing the cube data:
         - 'level': level number (int)
@@ -131,6 +183,7 @@ def read_cubes(it, path='', output_format = 'dictionaries'):
             - 'ini_ry', 'end_ry': initial and final y coordinates (float)
             - 'ini_rz', 'end_rz': initial and final z coordinates (float)
             - 
+        - if read_region is not None: list of cubes intersecting the region (ncubes not updated)
     """
 
     fname = 'cubes{:05d}'.format(it)
@@ -153,6 +206,51 @@ def read_cubes(it, path='', output_format = 'dictionaries'):
             this_level['ncubes'] = ncubes
             this_level['nmains'] = nmains
     
+            # read region
+            if read_region is not None:
+                # array marking cells in voids
+                mapOutput = read_void_map(it, path, output_marca=True, output_deltaTot=False, output_div=False)
+                marcaLev = mapOutput[0][ilev]
+                # run details to build grid to mask
+                void_details = read_void_catalogue(it, path, ret_catalogue=False)
+                levmin = void_details['levmin']
+                nmax = void_details['nmax'] #l = 0 grid size !!!
+                nmay = void_details['nmay']
+                nmaz = void_details['nmaz']
+                L0 = void_details['L0']
+                # mask of region
+                mask = np.zeros_like(marcaLev, dtype=int)
+                nx_lev = nmax * 2**(lev-levmin)
+                ny_lev = nmay * 2**(lev-levmin)
+                nz_lev = nmaz * 2**(lev-levmin)
+                dx_lev = L0 / nx_lev
+                dy_lev = L0 / ny_lev
+                dz_lev = L0 / nz_lev
+                x = np.arange(-L0/2+dx_lev/2, L0/2, dx_lev)
+                y = np.arange(-L0/2+dy_lev/2, L0/2, dy_lev)
+                z = np.arange(-L0/2+dz_lev/2, L0/2, dz_lev)
+                X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+
+                region_type = read_region[0]
+                if region_type == 'sphere':
+                    cx, cy, cz, R = read_region[1:]
+                    mask = (X-cx)**2 + (Y-cy)**2 + (Z-cz)**2 < R**2
+
+                elif region_type == 'box':
+                    x1, x2, y1, y2, z1, z2 = read_region[1:]
+                    mask = (X >= x1) & (X <= x2) & (Y >= y1) & (Y <= y2) & (Z >= z1) & (Z <= z2)
+
+                elif region_type == 'box_cw':
+                    xc, yc, zc, width = read_region[1:]
+                    mask = (np.abs(X-xc) < width) & (np.abs(Y-yc) < width) & (np.abs(Z-zc) < width)
+
+                else:
+                    raise ValueError('Error! region_type must be either "sphere", "box" or "box_cw"')
+
+                # mask voids inside region
+                voids_inside = np.unique(marcaLev[mask])    
+
+
             # cube data
             cubevoids=[]
             for iv in range(ncubes):
@@ -160,6 +258,9 @@ def read_cubes(it, path='', output_format = 'dictionaries'):
                 data_line = np.array(cube_catalogue.readline().split()).astype('f4')
                 C['id'] = int(data_line[0])
                 C['uvoid'] = int(data_line[1])
+                if read_region is not None:
+                    if C['uvoid'] not in voids_inside:
+                        continue
                 C['ini_ix'] = int(data_line[2])
                 C['end_ix'] = int(data_line[3])
                 C['ini_iy'] = int(data_line[4])
@@ -187,7 +288,8 @@ def read_cubes(it, path='', output_format = 'dictionaries'):
 
 def read_void_map(it, path='', output_marca=True, output_deltaTot=True, output_div=False):
     """
-    Reads the 3D arrays (map) file
+    Reads the 3D arrays (map) file containing the full void shapes from AVISM and
+    the density and velocity divergence fields used to perform the void-finding algorithm.
 
     Args:
         it: iteration number (int)
